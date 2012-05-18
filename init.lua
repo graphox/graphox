c2eeae3d0545ded856f51c654eda848a9e8498b4
killme <bram.wall@gmail.com>
2012-05-18 18:49:36 +0200

    added ldbus library

--- init.lua
+++ init.lua
@@ -1,10 +1,34 @@
 dofile "script/serialize.lua"
 
+package.path = package.path .. ";script/package/?.lua;"
+package.cpath = package.cpath .. ";lib/lib?.so"
+package.cpath = package.cpath .. ";lib/l?.so"
+
+require "ldbus"
+
+local connection = ldbus.bus ('session')
+
+ldbus.iterate()
+connection:call({
+	bus = "org.bansheeproject.Banshee",
+	path = "/org/bansheeproject/Banshee/PlayerEngine",
+	interface = "org.bansheeproject.Banshee.PlayerEngine",
+	method = 'GetProperties',	
+}, function(msg) print(msg); print(serialize_data(msg)) end)
+ldbus.iterate()
+
 print(vars.version())
 
+i = 0
 event = {
 	frame = function()
-
+		i = i + 1
+		if i == 200 then
+			i = 0
+			ldbus.iterate()
+			print("dbus!")
+			
+		end
 	end,
 	
 	connect = function(cn)
